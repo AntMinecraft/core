beef3157ec6ec502930f18160a7f26b6738a1e18
antonio32a <~@antonio32a.com>
2023-06-02 21:33:01 +0200

    Implement character width calculation and Formatting.splitComponent utils

--- src/main/java/com/antonio32a/core/util/Formatting.java
+++ src/main/java/com/antonio32a/core/util/Formatting.java
@@ -1,15 +1,38 @@
 package com.antonio32a.core.util;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import lombok.Data;
+import net.kyori.adventure.key.Key;
 import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.Style;
+import net.kyori.adventure.text.format.TextDecoration;
 import net.kyori.adventure.text.minimessage.MiniMessage;
 import net.kyori.adventure.text.minimessage.tag.resolver.TagResolver;
+import net.kyori.adventure.text.serializer.plain.PlainTextComponentSerializer;
+import net.minecraft.util.Tuple;
+import org.apache.commons.io.IOUtils;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.io.InputStream;
+import java.lang.reflect.Type;
+import java.nio.charset.StandardCharsets;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.zip.GZIPInputStream;
 
 public final class Formatting {
+    private static final Gson gson = new Gson();
+    private static final PlainTextComponentSerializer plainTextSerializer = PlainTextComponentSerializer.plainText();
     private static final MiniMessage miniMessage = MiniMessage.builder()
         .strict(true)
         .tags(TagResolver.standard())
         .build();
+
+    private static final HashMap<CharacterInfo, Integer> characterWidthMap = getCharacterWidthMap();
 
     private Formatting() {}
 
@@ -34,5 +57,122 @@ public final class Formatting {
     @NotNull
     public static Component parse(@NotNull String text, @NotNull TagResolver... resolvers) {
         return miniMessage.deserialize(text, resolvers);
+    }
+
+    /**
+     * Splits a component into a list of tuples containing the text and style of each part.
+     * The tuple's A value is the text, and the tuple's B value is the style.
+     *
+     * @param component The component to split.
+     * @return The list of tuples.
+     */
+    @NotNull
+    public static List<Tuple<String, Style>> splitComponent(@NotNull Component component) {
+        List<Tuple<String, Style>> result = component.children()
+            .stream()
+            .map(Formatting::splitComponent)
+            .flatMap(List::stream)
+            .collect(Collectors.toList());
+
+        Component newComponent = component.children(Collections.emptyList());
+        result.add(0, new Tuple<>(plainTextSerializer.serialize(newComponent), newComponent.style()));
+        return result;
+    }
+
+    /**
+     * Calculates the actual width of a component which ignores spacing between characters.
+     *
+     * @param component The component to calculate the width of.
+     * @return The actual width of the component.
+     */
+    public static int calculateActualWidth(@NotNull Component component) {
+        Integer charCount = splitComponent(component)
+            .stream()
+            .map(c -> c.getA().length())
+            .reduce(Integer::sum)
+            .orElseThrow();
+
+        return calculateWidth(component) - charCount;
+    }
+
+    /**
+     * Calculates the total width of a component.
+     *
+     * @param component The component to calculate the width of.
+     * @return The width of the component.
+     */
+    public static int calculateWidth(@NotNull Component component) {
+        List<CharacterInfo> characters = splitComponent(component).stream()
+            .flatMap(tuple -> {
+                StyleInfo style = StyleInfo.fromStyle(tuple.getB());
+                return tuple.getA().chars().mapToObj(c ->
+                    new CharacterInfo((char) c, style)
+                );
+            }).toList();
+
+        int width = 0;
+        for (CharacterInfo charInfo : characters) {
+            @Nullable Integer charWidth = characterWidthMap.get(charInfo);
+            if (charWidth == null) {
+                throw new IllegalStateException("Failed to get character width for character " + charInfo);
+            }
+            width += charWidth;
+        }
+        return width;
+    }
+
+    /**
+     * Gets the character width map from the character_widths.json file.
+     *
+     * @return The character width map.
+     */
+    private static HashMap<CharacterInfo, Integer> getCharacterWidthMap() {
+        try (InputStream stream = Formatting.class.getClassLoader().getResourceAsStream("character_widths.json.gz")) {
+            if (stream == null) {
+                throw new IllegalStateException("Failed to load character widths, stream is null.");
+            }
+
+            HashMap<CharacterInfo, Integer> result = new HashMap<>();
+            GZIPInputStream gzipStream = new GZIPInputStream(stream);
+            String text = IOUtils.toString(gzipStream, StandardCharsets.UTF_8);
+            Type listType = new TypeToken<List<CharacterWidth>>() {}.getType();
+            List<CharacterWidth> characterWidths = gson.fromJson(text, listType);
+            for (CharacterWidth characterWidth : characterWidths) {
+                result.put(
+                    new CharacterInfo(
+                        characterWidth.getCharacter(),
+                        characterWidth.getStyle()
+                    ),
+                    characterWidth.getWidth()
+                );
+            }
+            return result;
+        } catch (Exception exception) {
+            throw new IllegalStateException("Failed to load character widths", exception);
+        }
+    }
+
+    @Data
+    private static class CharacterWidth {
+        private final char character;
+        private final StyleInfo style;
+        private final int width;
+    }
+
+    @Data
+    private static class CharacterInfo {
+        private final char character;
+        private final StyleInfo style;
+    }
+
+    @Data
+    private static class StyleInfo {
+        private final boolean bold;
+        @Nullable private final String font;
+
+        public static StyleInfo fromStyle(Style style) {
+            @Nullable Key font = style.font();
+            return new StyleInfo(style.hasDecoration(TextDecoration.BOLD), font == null ? null : font.asString());
+        }
     }
 }
---
+++ src/test/java/com/antonio32a/core/util/FormattingTests.java
@@ -0,0 +1,98 @@
+package com.antonio32a.core.util;
+
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.event.HoverEvent;
+import net.kyori.adventure.text.format.NamedTextColor;
+import net.kyori.adventure.text.format.Style;
+import net.kyori.adventure.text.format.TextDecoration;
+import net.minecraft.util.Tuple;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+class FormattingTests {
+    @Test
+    void testSplitComponentBasic() {
+        Component component = Formatting.parse("unformatted text");
+        List<Tuple<String, Style>> result = Formatting.splitComponent(component);
+
+        assertEquals("unformatted text", result.get(0).getA());
+        assertEquals(Style.empty(), result.get(0).getB());
+    }
+
+    @Test
+    void testSplitComponentStyle() {
+        Component component = Formatting.parse("unformatted<red>red text</red>unformatted again");
+        List<Tuple<String, Style>> result = Formatting.splitComponent(component);
+
+        assertEquals("unformatted", result.get(0).getA());
+        assertEquals(Style.empty(), result.get(0).getB());
+
+        assertEquals("red text", result.get(1).getA());
+        assertEquals(Style.empty().color(NamedTextColor.RED), result.get(1).getB());
+
+        assertEquals("unformatted again", result.get(2).getA());
+        assertEquals(Style.empty(), result.get(2).getB());
+    }
+
+    @Test
+    void testSplitComponentMultipleStyles() {
+        Component component = Formatting.parse("unformatted<red><bold>bold red</bold></red>unformatted again");
+        List<Tuple<String, Style>> result = Formatting.splitComponent(component);
+
+        assertEquals("unformatted", result.get(0).getA());
+        assertEquals(Style.empty(), result.get(0).getB());
+
+        assertEquals("bold red", result.get(1).getA());
+        assertEquals(Style.empty().color(NamedTextColor.RED).decorate(TextDecoration.BOLD), result.get(1).getB());
+
+        assertEquals("unformatted again", result.get(2).getA());
+        assertEquals(Style.empty(), result.get(2).getB());
+    }
+
+    @Test
+    void testSplitComponentHoverStyle() {
+        Component component = Formatting.parse("unformatted<hover:show_text:'<red>test</red>'>hi</hover>unformatted again");
+        List<Tuple<String, Style>> result = Formatting.splitComponent(component);
+
+        assertEquals("unformatted", result.get(0).getA());
+        assertEquals(Style.empty(), result.get(0).getB());
+
+        assertEquals("hi", result.get(1).getA());
+        assertEquals(
+            Style.style()
+                .hoverEvent(HoverEvent.showText(Formatting.parse("<red>test</red>")))
+                .build(),
+            result.get(1).getB()
+        );
+
+        assertEquals("unformatted again", result.get(2).getA());
+        assertEquals(Style.empty(), result.get(2).getB());
+    }
+
+    @Test
+    void testCalculateWidthBasic() {
+        assertEquals(8, Formatting.calculateWidth(Component.text("hi")));
+    }
+
+    @Test
+    void testCalculateWidthStyle() {
+        assertEquals(8, Formatting.calculateWidth(Component.text("hi").style(Style.style().color(NamedTextColor.RED).build())));
+        assertEquals(10, Formatting.calculateWidth(Formatting.parse("<b>hi</b>")));
+    }
+
+    @Test
+    void testCalculateWidthCustomUnicode() {
+        assertEquals(180, Formatting.calculateWidth(Component.text("\uFB00")));
+    }
+
+    @Test
+    void testCalculateActualWidth() {
+        assertEquals(6, Formatting.calculateActualWidth(Component.text("hi")));
+        assertEquals(8, Formatting.calculateActualWidth(Formatting.parse("<b>hi</b>")));
+        assertEquals(179, Formatting.calculateActualWidth(Component.text("\uFB00")));
+    }
+}
